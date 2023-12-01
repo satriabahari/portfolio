fe24d8ff92a81c62cc16d1f53f23902d890433e6
Satria Bahari <satriaabaharii@gmail.com>
2023-12-01 22:02:46 +0700

    add folder constant

---
+++ constant/menu.tsx
@@ -0,0 +1,24 @@
+import { MenuItemProps } from "@/types/menu";
+
+export const MENU_ITEMS: MenuItemProps[] = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/#about",
+  },
+  {
+    title: "Skill",
+    href: "/#skill",
+  },
+  {
+    title: "Project",
+    href: "/#project",
+  },
+  {
+    title: "Contact",
+    href: "/#contact",
+  },
+];
