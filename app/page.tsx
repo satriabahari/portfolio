98524e8c67e8068a636065dc08416d037be0729e
Satria Bahari <satriaabaharii@gmail.com>
2023-12-01 22:07:29 +0700

    add HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Container from "@/components/Container";
+import About from "@/components/about";
 import Home from "@/components/home";
+import Skills from "@/components/skills";
 
 export default function HomePage() {
   return (
     <main>
       <Container>
         <Home />
+        <About/>
+        <Skills/>
       </Container>
     </main>
   );
