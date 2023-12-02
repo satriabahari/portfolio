9909f9acb7319e97489b9450c99fa861e485ca18
Satria Bahari <satriaabaharii@gmail.com>
2023-12-02 17:51:31 +0700

    Add types projects

---
+++ types/projects.ts
@@ -0,0 +1,9 @@
+export type ProjectsProps = {
+  id: number,
+  title: string,
+  description: string,
+  image: string,
+  link_demo?: string | null,
+  link_github?: string | null,
+  stacks: string[],
+}
