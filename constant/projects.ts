461304370073882e0a63346eea8d7f979b58e15a
Satria Bahari <satriaabaharii@gmail.com>
2023-12-02 17:50:47 +0700

    Add constant projects

---
+++ constant/projects.ts
@@ -0,0 +1,40 @@
+import { ProjectsProps } from "@/types/projects";
+
+export const PROJECTS: ProjectsProps[] = [
+  {
+    id: 1,
+    title: "Website RPL",
+    description: "Rekayasa Perangkat Lunak",
+    image: "/project_1.png",
+    link_demo: "",
+    link_github: "",
+    stacks: ["Next.js", "TailwindCSS"],
+  },
+  {
+    id: 2,
+    title: "Website RPL",
+    description: "Rekayasa Perangkat Lunak",
+    image: "/project_1.png",
+    link_demo: "",
+    link_github: "",
+    stacks: ["Next.js", "TailwindCSS"],
+  },
+  {
+    id: 3,
+    title: "Website RPL",
+    description: "Rekayasa Perangkat Lunak",
+    image: "/project_1.png",
+    link_demo: "",
+    link_github: "",
+    stacks: ["Next.js", "TailwindCSS"],
+  },
+  {
+    id: 4,
+    title: "Website RPL",
+    description: "Rekayasa Perangkat Lunak",
+    image: "/project_1.png",
+    link_demo: "",
+    link_github: "",
+    stacks: ["Next.js", "TailwindCSS"],
+  },
+];
