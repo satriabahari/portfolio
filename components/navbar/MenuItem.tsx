07da175ed10dc98a1e4a7d54b3258e98b4621c10
Satria Bahari <satriaabaharii@gmail.com>
2024-01-11 19:23:48 +0700

    fix code navbar

--- components/navbar/MenuItem.tsx
+++ components/navbar/MenuItem.tsx
@@ -1,11 +1,21 @@
+"use client";
+
+import { selectedActiveSection } from "@/lib/features/menu/menuSlice";
+import { useAppSelector } from "@/lib/hooks";
 import { MenuItemProps } from "@/types/menu";
 import Link from "next/link";
-import React from "react";
 
 export default function MenuItem({ title, href }: MenuItemProps) {
+  const isActive = useAppSelector(selectedActiveSection);
+
   return (
     <div>
-      <Link href={href} className="text-neutral-400 hover:text-neutral-100">
+      <Link
+        href={href}
+        className={`text-neutral-400 hover:text-neutral-100 ${
+          isActive.toLowerCase() === title.toLowerCase() ? "text-blue-500" : null
+        }`}
+      >
         {title}
       </Link>
     </div>
