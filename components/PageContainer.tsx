8b902d5c3cb901ea4288e4c05a00d93dd396dc18
Satria Bahari <satriaabaharii@gmail.com>
2024-01-11 19:23:08 +0700

    add prop sectionName

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -1,14 +1,31 @@
+"use client";
+
+import { setActiveSection } from "@/lib/features/menu/menuSlice";
+import { useAppDispatch } from "@/lib/hooks";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
 type PageContainerProps = {
   children: React.ReactNode;
   sidePadding?: boolean;
+  sectionName: string;
 };
 
 export default function PageContainer({
   children,
   sidePadding,
+  sectionName,
 }: PageContainerProps) {
+  const { ref, inView } = useInView({ threshold: 0 });
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (inView) {
+      dispatch(setActiveSection(sectionName));
+    }
+  }, [dispatch, inView, sectionName]);
   return (
     <section
+      ref={ref}
       className={`flex min-h-screen w-full flex-col items-center justify-center ${
         sidePadding ? "px-48" : ""
       }`}
