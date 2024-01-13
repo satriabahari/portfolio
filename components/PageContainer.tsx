4e10e95044fc6a73861a9ebddb2c2e85a586025e
Satria Bahari <satriaabaharii@gmail.com>
2024-01-13 08:18:21 +0700

    Refactor: Added some props to the page container component

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -8,15 +8,19 @@ import { useInView } from "react-intersection-observer";
 type PageContainerProps = {
   children: React.ReactNode;
   sidePadding?: boolean;
+  noHFull?: boolean;
   sectionName: string;
+  threshold?: number;
 };
 
 export default function PageContainer({
   children,
   sidePadding,
   sectionName,
+  noHFull,
+  threshold = 0.75,
 }: PageContainerProps) {
-  const { ref, inView } = useInView({ threshold: 0.75 });
+  const { ref, inView } = useInView({ threshold });
   const dispatch = useAppDispatch();
   useEffect(() => {
     if (inView) {
@@ -26,9 +30,9 @@ export default function PageContainer({
   return (
     <section
       ref={ref}
-      className={`flex min-h-screen w-full flex-col items-center justify-center ${
+      className={`flex w-full flex-col items-center justify-center ${
         sidePadding ? "px-48" : ""
-      }`}
+      } ${noHFull ? "" : "h-screen"}`}
     >
       {children}
     </section>
