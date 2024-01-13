0c540f126cfb552586e95faf0a4e0557368d7642
Satria Bahari <satriaabaharii@gmail.com>
2024-01-13 08:24:16 +0700

    Refactor: Added some props to the page container in the project

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -5,7 +5,7 @@ import PageContainer from "../PageContainer";
 
 export default function Projects() {
   return (
-    <PageContainer sectionName="Projects">
+    <PageContainer sectionName="Projects" threshold={0.5} noHFull>
       <PageHeading title="Projects" />
       <ProjectList />
     </PageContainer>
