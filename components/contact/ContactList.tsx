d3d0049a1a0545ac5a2f6e983e097cc111e1e80e
Satria Bahari <satriaabaharii@gmail.com>
2024-01-13 08:22:34 +0700

    Refactor: Fixed field on the contact

--- components/contact/ContactList.tsx
+++ components/contact/ContactList.tsx
@@ -1,7 +1,54 @@
-import { SOCIAL_MEDIA } from "@/constant/socialMedia";
-import clsx from "clsx";
+// import { SOCIAL_MEDIA } from "@/constant/socialMedia";
 import Link from "next/link";
 import React from "react";
+import { twMerge } from "tailwind-merge";
+
+import { SocialMediaProps } from "@/types/socialMedia";
+import {
+  BsGithub as GithubIcon,
+  BsInstagram as InstagramIcon,
+  BsLinkedin as LinkedinIcon,
+  BsTiktok as TiktokIcon,
+} from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+
+const iconSize = 20;
+
+export const SOCIAL_MEDIA = [
+  {
+    title: "Gmail",
+    href: "mailto:satriaabaharii@gmail.com",
+    icon: <SiGmail size={iconSize} className="text-red-500" />,
+    backgroundColor: "bg-gradient-to-b from-neutral-200 to-neutral-400",
+    textColor: "text-red-500",
+  },
+  {
+    title: "Instagram",
+    href: "https://www.instagram.com/satriabaharii_/",
+    icon: <InstagramIcon size={iconSize} />,
+    backgroundColor:
+      "bg-gradient-to-b from-purple-700 via-pink-500 to-orange-500",
+  },
+  {
+    title: "Linkedin",
+    href: "https://www.linkedin.com/in/satria-bahari/",
+    icon: <LinkedinIcon size={iconSize} />,
+    backgroundColor: "bg-gradient-to-b from-sky-700 to-sky-950",
+  },
+  {
+    title: "Tiktok",
+    href: "https://www.tiktok.com/@satriaabaharii/",
+    icon: <TiktokIcon size={iconSize} />,
+    backgroundColor: "bg-gradient-to-b from-neutral-700 to-neutral-900",
+  },
+  {
+    title: "Github",
+    href: "https://github.com/satriabahari",
+    icon: <GithubIcon size={iconSize} />,
+    backgroundColor: "bg-gradient-to-b from-neutral-200 to-neutral-500",
+    textColor: "text-neutral-900",
+  },
+];
 
 export default function ContactList() {
   return (
@@ -12,10 +59,12 @@ export default function ContactList() {
           className="rounded-lg border border-neutral-600 bg-neutral-900 p-1"
         >
           <Link
+            target="_blank"
             href={media.href}
-            className={clsx(
+            className={twMerge(
               "flex w-full items-center space-x-2 rounded-md px-4 py-2",
               media.backgroundColor,
+              media.textColor,
             )}
           >
             {media.icon}
