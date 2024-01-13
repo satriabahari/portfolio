cdfc4592665bd4539f17e4b1c745d82d67051634
Satria Bahari <satriaabaharii@gmail.com>
2024-01-13 08:25:40 +0700

    Fix: Fixed some constant

--- constant/stacks.tsx
+++ constant/stacks.tsx
@@ -7,6 +7,7 @@ import {
   SiGithub,
   SiHtml5,
   SiJavascript,
+  SiMysql,
   SiNextdotjs,
   SiNodedotjs,
   SiPhp,
@@ -25,75 +26,75 @@ export type stackProps = {
 const iconSize = "100%";
 
 export const STACKS: stackProps[] = [
+  // {
+  //   title: "HTML",
+  //   icon: <SiHtml5 size={iconSize} className="text-orange-500" />,
+  // },
+  // {
+  //   title: "CSS",
+  //   icon: <SiCss3 size={iconSize} className="text-blue-500" />,
+  // },
+  // {
+  //   title: "Bootstrap",
+  //   icon: <BsFillBootstrapFill size={iconSize} className="text-purple-500" />,
+  // },
+  // {
+  //   title: "TailwindCSS",
+  //   icon: <SiTailwindcss size={iconSize} className="text-cyan-300" />,
+  // },
+  // {
+  //   title: "PHP",
+  //   icon: <SiPhp size={iconSize} className="text-blue-500" />,
+  // },
+  // {
+  //   title: "JavaScript",
+  //   icon: <SiJavascript size={iconSize} className="text-yellow-400" />,
+  // },
+  // {
+  //   title: "TypeScript",
+  //   icon: <SiTypescript size={iconSize} className="text-blue-400" />,
+  // },
+  // {
+  //   title: "Node JS",
+  //   icon: <SiNodedotjs size={iconSize} className="text-green-600" />,
+  // },
+  // {
+  //   title: "Express.js",
+  //   icon: <SiExpress size={iconSize} />,
+  // },
+  // {
+  //   title: "React.js",
+  //   icon: <SiReact size={iconSize} className="text-sky-500" />,
+  // },
+  // {
+  //   title: "Vite",
+  //   icon: <SiVite size={iconSize} className="text-purple-500" />,
+  // },
+  // {
+  //   title: "Next.js",
+  //   icon: <SiNextdotjs size={iconSize} />,
+  // },
+  // {
+  //   title: "Framer Motion",
+  //   icon: <SiFramer size={iconSize} />,
+  // },
   {
-    title: "PHP",
-    icon: <SiPhp size={iconSize} className="text-blue-500" />,
-  },
-  {
-    title: "JavaScript",
-    icon: <SiJavascript size={iconSize} className="text-yellow-400" />,
-  },
-  {
-    title: "TypeScript",
-    icon: <SiTypescript size={iconSize} className="text-blue-400" />,
-  },
-  {
-    title: "Next.js",
-    icon: <SiNextdotjs size={iconSize} />,
-  },
-  {
-    title: "React.js",
-    icon: <SiReact size={iconSize} className="text-sky-500" />,
-  },
-  {
-    title: "TailwindCSS",
-    icon: <SiTailwindcss size={iconSize} className="text-cyan-300" />,
-  },
-  {
-    title: "Bootstrap",
-    icon: <BsFillBootstrapFill size={iconSize} className="text-purple-500" />,
-  },
-  {
-    title: "Vite",
-    icon: <SiVite size={iconSize} className="text-purple-500" />,
+    title: "MySQL",
+    icon: <SiMysql size={iconSize} className="text-blue-800"/>
   },
   {
     title: "PostgreSql",
     icon: <BiLogoPostgresql size={iconSize} className="text-blue-400" />,
   },
   {
-    title: "React Native",
-    icon: <SiReact size={iconSize} className="text-sky-600" />,
-  },
-  {
-    title: "Framer Motion",
-    icon: <SiFramer size={iconSize} />,
-  },
-  {
-    title: "Express.js",
-    icon: <SiExpress size={iconSize} />,
-  },
-  {
-    title: "HTML",
-    icon: <SiHtml5 size={iconSize} className="text-orange-500" />,
-  },
-  {
-    title: "CSS",
-    icon: <SiCss3 size={iconSize} className="text-blue-500" />,
-  },
-  {
     title: "Prisma",
     icon: <SiPrisma size={iconSize} className="text-teal-500" />,
-  },
-  {
-    title: "Node JS",
-    icon: <SiNodedotjs size={iconSize} className="text-green-600" />,
   },
   {
     title: "Github",
     icon: <SiGithub size={iconSize} />,
   },
-];
+] as const;
 
 // type stacksProps = {
 //   [key: string]: JSX.Element;
