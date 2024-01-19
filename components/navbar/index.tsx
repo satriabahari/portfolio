2a247d4de0f65f01ca876f1666b9075f15382e15
Satria Bahari <satriaabaharii@gmail.com>
2024-01-19 20:15:18 +0700

    Style: Changed z-index

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -7,7 +7,7 @@ import HamburgerToggle from "./HamburgerToggle";
 
 export default function Navbar() {
   return (
-    <header className="fixed z-50 lg:left-1/2 lg:top-4 lg:-translate-x-1/2">
+    <header className="fixed z-10 lg:left-1/2 lg:top-4 lg:-translate-x-1/2">
       <div className=" flex w-screen items-center justify-between gap-x-24 border-b-2 border-neutral-400 bg-neutral-200 px-8 py-3 lg:w-fit lg:justify-center lg:rounded-full lg:border-2 dark:border-neutral-700 dark:bg-neutral-900">
         <Profile />
         <Menu list={MENU_ITEMS} />
