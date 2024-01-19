ddf5c7bcd7d45eddb379d6016abfbe3d9991948e
Satria Bahari <satriaabaharii@gmail.com>
2024-01-19 11:14:05 +0700

    Style: Added class scroll-smooth in tag html

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <body className={soraSans.className}>
         <StoreProvider>
           <ThemeProviderContext>
