e962355c2f581c2ee588bb94e017388d03edeb7e
Satria Bahari <satriaabaharii@gmail.com>
2024-01-19 20:16:04 +0700

    Feat: Added Modal project card

--- components/projects/ProjectList.tsx
+++ components/projects/ProjectList.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
 import { PROJECTS } from "@/constant/projects";
+import ProjectModal from "./ProjectModal";
 
 export default function ProjectList() {
   return (
     <div className="grid w-full gap-x-8 gap-y-8 px-8 lg:grid-cols-3">
       {PROJECTS.map((project, index) => (
-        <ProjectCard key={index} {...project} />
+        <ProjectCard
+          key={index}
+          {...project}
+        />
       ))}
     </div>
   );
